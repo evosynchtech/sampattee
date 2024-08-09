9f6274901fb15d6e1e0a6e4d71042e8b70b64eed
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-08-09 17:47:45 +0530

    Built the website on react

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
