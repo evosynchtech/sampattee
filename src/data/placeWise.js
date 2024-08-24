0d0fc78bfce9f0cfd8c87fc42e6be167236d5198
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-08-24 18:24:36 +0530

    Added: Placewise property details

---
+++ src/data/placeWise.js
@@ -0,0 +1,119 @@
+const placeWise = {
+  banur: [
+    {
+      title: "Chandigarh Royale City (Details & Prices)- Banur",
+      description:
+        "Chandigarh Royale City is a special, unique township project in the Tricity, created in accordance with the State Government's unique township policy. The goal of this project is to create a landmark that will serve as a future reference point with areas and concepts tailored to each family member's needs. Additionally, this endeavor offers the finest aspects of what makes life joyful. Over 60% of Chandigarh Royale City's 200 acres are made up of open area or vegetation. Compared to the other surrounding housing projects, this one is distinct.",
+    },
+  ],
+  dera_bassi: [
+    {
+      title: "Motia Chandigarh Hills (Price & Project Details) – Dera Bassi",
+      description:
+        "Motia Chandigarh Hills Dera Bassi is undoubtedly one of the most sought-after residential societies in the area. It is situated at a distance of merely 2 minutes from the Chandigarh-Ambala highway. Spanning over 13 acres of land, the society offers residents a peaceful and serene environment. The lavish property is ideal for keeping you away from the hustle and bustle of city life. With all plots being Vastu compliant and Either North-facing or East-facing, the society is an ideal choice for those looking to build their dream home.",
+    },
+    {
+      title: "SBP Housing Park (1/2/3 BHK Apartments & Prices)- Dera Bassi",
+      description:
+        "For the luxurious and cosmopolitan lifestyle that you imagine for yourself and your family, SBP brings Housing Park to Derabassi- the industrial belt of Tricity. This 20-acre project offers not only futuristic designs but also ensures that one enjoys living close to nature every second of their life.",
+    },
+    {
+      title: "SBP Housing Park (1/2/3 BHK Apartments & Prices)- Dera Bassi",
+      description:
+        "For the luxurious and cosmopolitan lifestyle that you imagine for yourself and your family, SBP brings Housing Park to Derabassi- the industrial belt of Tricity. This 20-acre project offers not only futuristic designs but also ensures that one enjoys living close to nature every second of their life.",
+    },
+    {
+      title: "Kings Valley by Ubber (Project Details & Prices)- Derabassi",
+      description:
+        "For those who love to live the grand life like kings and queens, Ubber Buildcon presents Kings Valley in Derabassi. This property presents residential plots that let you build the home you always wanted.",
+    },
+    {
+      title: "ATS Golf Meadows, Dera Bassi",
+      description:
+        "ATS Golf Meadows is a 300-acre township located at the gateway of Chandigarh. The township is skillfully designed for advanced comfort levels. The architectural design of the township is prepared by U.S Architect Oru Bose. The design of the township properly incorporates a green environment, stylish architecture, and convenient amenities. The core idea of the community depends on installing verdant green pathways and constructing aesthetically designed friendly living spaces.",
+    },
+  ],
+  zirakpur: [
+    {
+      title: "Harmony Imperial Apartments: Luxury Lifestyle",
+      description:
+        "Developed by Harmony Colonizers Pvt Ltd, Imperial Apartments in Zirakpur are known for their high quality and affordability. This real estate company is registered and certified with RERA. Their portfolio includes townships, residential complexes, and premium projects.",
+    },
+    {
+      title: "KSB Dream Homes (Price & Project Details) – Zirakpur",
+      description:
+        "KSB Dream Homes is a much-awaited housing society in Zirakpur, Mohali, that has finally opened its doors to residents. Families looking for a comfortable and fashionable home will find this opulent and roomy building an excellent choice because it provides 3 BHK apartments at very reasonable pricing. All of the social infrastructures in and around the city are easily accessible due to the project’s advantageous location.",
+    },
+    {
+      title: "GHB Splande -Details, Price, and Everything You Need to Know",
+      description:
+        "Looking for a better housing community to invest your hard-earned money in? Go no further; check out GHB Splande. It is an under-construction housing society in Zirakpur. There are independent floors and apartments for sale. This community will have all the fundamental amenities and facilities that will perfectly suit your needs.",
+    },
+    {
+      title: "Aster Plaza: The Future of Commercial Places",
+      description:
+        "Aster Plaza is an amalgamation of artistically and practically designed showrooms that will be a perfect space for all modern brands. It is currently under construction, and the possession will initiate in December 2024. The main benefit of this commercial project is the low-cost maintenance of the building; that is, the investors don’t have to pay much for the maintenance of their space.",
+    },
+    {
+      title: "All About NK Savitry Towers Zirakpur",
+      description:
+        "NK Savitry Towers is a ready-to-move housing community in Zirakpur. It has several apartments with a wide budget range. The flats are a perfect amalgamation of comfort and style specially designed to suit your status and convenience. Now that’s what we call a perfect home.",
+    },
+  ],
+  new_chandigarh: [
+    {
+      title: "Omaxe The Resort (Price & Project Details)- New Chandigarh",
+      description:
+        "Are you looking for a real estate property in Chandigarh? If so, you may want to consider the Omaxe The Resort. Spread over nine acres of lush land; this integrated township has all modern luxuries, including a clubhouse, gymnasium, swimming pool, tennis court, etc. Whether you’re looking for a luxurious penthouse or an affordable starter home, it has it all.",
+    },
+    {
+      title: "The Ananta Aspire (Price & Project Details) – New Chandigarh",
+      description:
+        "The Ananta Aspire is a one-of-a-kind residential complex that blends luxury, comfort, style and technology to create happy homes. Spread over 34,050 square yards, the residence offers an exclusive inventory of 4+1, 3+1 and 3 BHK smart homes for sale in Zirakpur, New Chandigarh.",
+    },
+    {
+      title: "Omaxe The Lake (Project Details & Price)",
+      description:
+        "The combination of luxury and convenience in the right amounts is the key to a fruitful and comfortable life. With a strong belief in providing both, Omaxe presents The Lake, an ultra-luxurious residential project lying in the foothills of Shivalik Hills and right in New Chandigarh.",
+    },
+    {
+      title: "Suntec City (Project Details & Prices)",
+      description:
+        "In the hustle of life, we often carve for the beautiful mountains filled with infinite flora and fauna. Imagine if you could get it all while still living in the city. Seems unreal, right? Well, with Suntec City, this is possible.",
+    },
+    {
+      title: "Palm Residency (Price & Project Details)- New Chandigarh",
+      description:
+        "Imagine a life where you can come home to your little piece of paradise, surrounded by the green landscape of Chandigarh. This is what the Palm Residency promises its residents – a luxurious haven away from the hustle and bustle of city life. Located in Mullanpur, New Chandigarh, the Palm Residency is a gated community of independent premium floors designed for those who appreciate the superior things in life.",
+    },
+  ],
+  kharar: [
+    {
+      title: "PNB Picasso Floors (Price & Project Details) – Kharar",
+      description:
+        "Do you ever wish to live a life of luxury? Well, your search ends here! Introducing PNB Picasso Floors – a unique and innovative offering that provides luxurious and affordable flats in Mohali. This property offers panoramic views of the bustling city skyline from the comfort of your home. What’s more interesting is that the property is exceptionally affordable 1BHK and 2BHK independent floors.",
+    },
+    {
+      title: "Palm Village -Project Details, Specifications, and Features",
+      description:
+        "Palm Village is a residential project by Bee Gee Buildtech in Kharar Mohali. It has apartments with a perfect combination of contemporary infrastructure and amenities to provide comfortable living.",
+    },
+    {
+      title: "MVD Nature City (Price & Project Details)- Kharar",
+      description:
+        "Ever wondered if luxury and budget could go hand in hand? MVD Nature City Kharar could be exactly what you’ve been looking for. Spread over 2 acres, it is a beautiful 2BHK and 3BHK residential project by MVD Buildwell Private Limited. The well-planned layouts, open spaces, and easy access to all conveniences make this property an ideal place to call home.",
+    },
+    {
+      title: "Aroma Heights (Price & Project Details) – Kharar",
+      description:
+        "Aroma Heights has been one of the most inspiring ventures when it comes to providing privileges and luxuries. The housing project is a dream home for those looking for a luxurious lifestyle. Developed by Aroma Developers, the residency offers exclusive 2 and 3 BHK apartments.",
+    },
+    {
+      title: "Ubber Mews Gate- Project Details, Amenities, and Locality",
+      description:
+        "Ubber Mews Gate, a phenomenally created masterpiece to reflect your aspirations. A perfect combination of cosy, luxurious, and serene. It has taken contemporary living to an entirely new level by providing an environment that’s perfect for a modern family lifestyle.",
+    },
+  ],
+};
+
+export default placeWise;
--- src/pages/ShowPlaceWise.jsx
+++ src/pages/ShowPlaceWise.jsx
@@ -2,46 +2,54 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from "../components/buttons/Button";
 import data from "../data/Data";
+import placeWise from "../data/placeWise";
 
 function ShowPlaceWise() {
-  let [filteredData, setFilteredData] = useState();
+  let [filteredData, setFilteredData] = useState([]);
+  let [place, setPlace] = useState("");
   const param = useParams();
   let placeName = param.id;
+  setPlace(placeName);
+  useEffect(() => {
+    setFilteredData(placeWise[placeName]);
+  }, []);
+  console.log(filteredData);
   placeName = placeName.split("_").join(" ");
-  //   console.log(str)
   return (
     <div className="place-wise-property-container w-full bg-black flex flex-col items-center justify-center">
       <div className="max-w-[1000px] bg-gray-950 rounded-xl px-6">
-        <div className="w-full">
+        <div className="w-full my-4">
           <h1 className="mb-4 w-full capitalize text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             {placeName}
           </h1>
         </div>
-        <div className="w-full">
-          <div class="p-4 bg-black rounded-xl border border-white">
-            <div class="h-full flex sm:flex-row flex-col sm:items-start items-center sm:justify-start justify-center text-center sm:text-left">
-              <img
-                alt="team"
-                class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                src={`/assets/${placeName}/.jpg`}
-              />
-              <div class="flex flex-col gap-2 sm:pl-8">
-                <h2 class="title-font font-medium text-lg text-white">
-                  PNB Picasso Floors (Price & Project Details) – Kharar
-                </h2>
-                <p class="mb-4 text-slate-500 truncate-ellipsis">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
-                  amet minima numquam possimus reiciendis sint doloribus sed
-                  autem temporibus impedit. Temporibus animi molestias, enim cum
-                  magni eum sint optio harum provident minus doloribus pariatur
-                </p>
-                <div class="inline-flex">
-                  <Button width={30} title={"Read More"} marginX={2} />
+        {filteredData.length > 0 &&
+          filteredData.map((data) => (
+            <>
+              <div className="w-full my-4 sm:my-8">
+                <div class="p-4 bg-black rounded-xl border border-white">
+                  <div class="h-full flex sm:flex-row flex-col sm:items-start items-center sm:justify-start justify-center text-center sm:text-left">
+                    <img
+                      alt="team"
+                      class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                      src={`/assets/${placeName}/.jpg`}
+                    />
+                    <div class="flex flex-col gap-2 sm:pl-8">
+                      <h2 class="title-font font-medium text-lg text-white">
+                        {data.title}
+                      </h2>
+                      <p class="mb-4 text-slate-500 truncate-ellipsis">
+                        {data.description}
+                      </p>
+                      <div class="inline-flex">
+                        <Button width={30} title={"Read More"} marginX={2} />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
+            </>
+          ))}
       </div>
     </div>
   );
