cc8a4dac32a9898890a9149136848d6fbcbbd489
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-08-26 10:31:23 +0530

    Added: Heading in Trending and Featured Sections

--- src/components/FeaturedProperties/FeaturedPropertyCard.jsx
+++ src/components/FeaturedProperties/FeaturedPropertyCard.jsx
@@ -2,16 +2,21 @@ import React from "react";
 
 function FeaturedPropertyCard({ imgUrl, title, description }) {
   return (
-    <div class="hover:cursor-pointer group relative h-full w-full overflow-hidden rounded-lg shadow-lg">
-      <img
-        className="w-full h-auto block rounded-lg"
-        src={imgUrl}
-        alt={title}
-      />
-      <div class="absolute h-full bottom-0 left-0 right-0 bg-[#0000007a] text-white text-center p-5 transform group-hover:translate-y-0 translate-y-full transition-transform duration-300 ease-in-out">
-        <h3 className="m-0 text-[1.2em] font-bold">{title}</h3>
-        <p>{description}</p>
+    <div className="flex flex-col my-3">
+      <div class="hover:cursor-pointer group relative h-full w-full overflow-hidden rounded-lg shadow-lg">
+        <img
+          className="h-full w-full object-cover block rounded-lg"
+          src={imgUrl}
+          alt={title}
+        />
+        <div class="absolute h-full bottom-0 left-0 right-0 bg-[#0000007a] text-white text-center p-5 transform group-hover:translate-y-0 translate-y-full transition-transform duration-300 ease-in-out">
+          <h3 className="m-0 text-[1.2em] font-bold">{title}</h3>
+          <p>{description}</p>
+        </div>
       </div>
+      <h1 className="bg-black text-white text-start font-bold p-1 -mx-1">
+        {title}
+      </h1>
     </div>
   );
 }
--- src/components/trendingProjects/TrendingProjectCard.jsx
+++ src/components/trendingProjects/TrendingProjectCard.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 
-function TrendingProjectCard({imgUrl, title, description}) {
+function TrendingProjectCard({ imgUrl, title, description }) {
   return (
-    <div class="swiper-slide bg-black cursor-pointer">
+    <div class="swiper-slide bg-black cursor-pointer flex flex-col">
       <div class="trending-project-card overflow-hidden rounded-[10px] relative w-full h-full">
         <div class="trending-project-card-item flex justify-center w-full h-full group">
-          <img
-            className="h-full object-cover"
-            src={imgUrl}
-            alt={title}
-          />
+          <img className="h-full object-cover" src={imgUrl} alt={title} />
           <div class="trending-project-card-overlay h-full absolute bottom-0 left-0 right-0 bg-[#0000009a]  text-white text-center p-2 transform translate-y-full transition-transform duration-300 ease-in-out group-hover:translate-y-0">
-            <h3 className="m-0 text-[1.2em] font-bold">
-              {title}
-            </h3>
-            <p className="mt-3 text-[1em]">
-              {description}
-            </p>
+            <h3 className="m-0 text-[1.2em] font-bold">{title}</h3>
+            <p className="mt-3 text-[1em]">{description}</p>
           </div>
         </div>
       </div>
+      <h1 className="bg-black text-white text-start font-bold p-1 -mx-1">
+        {title}
+      </h1>
     </div>
   );
 }
